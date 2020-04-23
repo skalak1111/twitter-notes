56e76d5bab7bfe94398e060b6acd2e98f46a771a
Sai Krishna Kalakonda <saikrishna.kalakonda@ibm.com>
2020-04-22 22:57:47 -0400

    Connecting with Express and Server running

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello WOrld");
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("HO HO HO");
+})
+
+app.listen(3004, function(err) {
+    console.log("Express server is running on port 3004")
+})
